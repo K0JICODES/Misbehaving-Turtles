c178854e2f7e7a36ea3332f8ae7b478aa6a66f74
K0JICODES <k0jikodes@gmail.com>
2024-11-07 20:27:54 -0500

    Update Misbehaving_Turtles.py

--- Misbehaving_Turtles.py
+++ Misbehaving_Turtles.py
@@ -1,1 +1,31 @@
+'''Make it so the naughty turtle 25% of the time will turn in the opposite direction you tell it to.'''
 
+import turtle
+import random
+
+class MisbehavingTurtle(turtle.Turtle):
+    #You fill in the rest.
+
+
+# test case
+# drawing an octagon and a square
+def drawing_test(t):
+    '''drawing_test(t)
+     draws an octagon and square with t'''
+    for i in range(8):
+        t.forward(30)
+        t.left(45)
+    t.right(45)
+    for i in range(4):
+        t.forward(50)
+        t.right(90)
+        
+# one nice turtle and one not-so-nice turtle
+wn = turtle.Screen()
+sugar = turtle.Turtle()
+sugar.color('green')
+drawing_test(sugar)
+spice = MisbehavingTurtle()
+spice.color('red')
+drawing_test(spice)
+wn.mainloop()
